7b76912bced075d6277ec60f9f259f8c09cd9ef0
shrekl1234 <shrekl1234@users.noreply.github.com>
2020-07-28 18:39:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.set("view engine", "ejs");
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+app.listen(3000, ()=>{
+    console.log("server started at port 3000");
+});
