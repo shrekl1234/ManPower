1526f776cc15e157b1b82103b47850e7394ccba4
shrekl1234 <shrekl1234@users.noreply.github.com>
2020-08-04 10:52:34 +0530

    add all services -responsive -services-inside

--- app.js
+++ app.js
@@ -12,28 +12,61 @@ app.use(
 );
 app.set("view engine", "ejs");
 
-
-app.get("/",(req,res)=>{
-    res.render("index");
-});
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-app.get("/cooking", (req, res) => {
-  res.render("pages/cooking");
-});
-
-app.get("/cleaning", (req, res) => {
-  res.render("pages/service-not-found");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 
-app.listen(3000, ()=>{
-    console.log("server started at port 3000");
+app.get("/:service", (req, res) => {
+  const service = req.params.service;
+  if (service === "login") {
+    res.render("login");
+  }
+  if (service === "signup") {
+    res.render("signup");
+  }
+  if (service === "register") {
+    res.render("register");
+  }
+  if (service === "cooking") {
+    res.render("pages/cooking");
+  }
+  if (service === "cleaning") {
+    res.render("pages/cleaning");
+  }
+  if (service === "nurse") {
+    res.render("pages/nurse");
+  }
+  if (service === "baby-sitter") {
+    res.render("pages/baby-sitter");
+  }
+  if (service === "laundary") {
+    res.render("pages/laundary");
+  }
+  if (service === "water-cane-supply") {
+    res.render("pages/water-cane-supply");
+  }
+  if (service === "grocery-delivery") {
+    res.render("pages/grocery-delivery");
+  }
+  if (service === "trailor") {
+    res.render("pages/trailor");
+  }
+  if (service === "burtician") {
+    res.render("pages/burtician");
+  }
+  if (service === "meat-delivery") {
+    res.render("pages/meat-delivery");
+  }
+  if (service === "tutor") {
+    res.render("pages/service-not-found");
+  }
+  if (service === "tiffin-services") {
+    res.render("pages/tiffin-services");
+  }else{
+      res.render("pages/service-not-found");
+  }
+});
+app.listen(3000, () => {
+  console.log("server started at port 3000");
 });
